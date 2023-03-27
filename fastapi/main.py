1f06ef40f046429515ad8d908f4f3b5646cd306b
Furio0124 <47459062+Furio0124@users.noreply.github.com>
2023-03-27 22:18:24 +0700

    Add files via upload
    Edited files from the folder "pages": login, main.
    Added requests to the flask computing server and authorization. /fastpapi/main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,93 +1,30 @@
-import uuid
-from fastapi import FastAPI, Body, status
-from fastapi.responses import JSONResponse, FileResponse
-import json
+﻿from fastapi import FastAPI, Body
+from fastapi.responses import JSONResponse, Response
 import requests
- 
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.id = str(uuid.uuid4())
- 
-people = [Person("Tom", 38), Person("Bob", 42), Person("Sam", 28)]
-
-def find_person(id):
-   for person in people: 
-        if person.id == id:
-           return person
-   return None
+import uvicorn
+import sys
+sys.path.insert(1, '../')
+from utils.work_with_database import check_username, check_user_password
  
 app = FastAPI()
 
-
-@app.get("/")
-async def main():
+@app.post("/calc")
+def calc(data = Body()):
+    try:
+        res = requests.post(url='http://127.0.0.1:8051/calc', json=data) #отправка данных расчета на сервер вычислений
+        return res.status_code
+    except Exception:
+        return Response(content = 'Failure: invalid JSON!\n', media_type="text/plain", status_code=500)
     
-    return FileResponse("public/index.html")
- 
-@app.get("/api/users")
-def get_people():
-    return people
- 
-@app.get("/api/users/{id}")
-def get_person(id):
-    person = find_person(id)
-    print(person)
-    if person==None:  
-        return JSONResponse(
-                status_code=status.HTTP_404_NOT_FOUND, 
-                content={ "message": "Пользователь не найден" }
-        )
-    return person
- 
- 
-@app.post("/api/users")
-def create_person(data  = Body()):
-    #data["age"] = requ(data["age"],5)-----------------------connect with Flask and post request
-    person = Person(data["name"], data["age"])
-    people.append(person)
-    return person
- 
-@app.put("/api/users")
-def edit_person(data  = Body()):
-  
-    # получаем пользователя по id
-    person = find_person(data["id"])
-    # если не найден, отправляем статусный код и сообщение об ошибке
-    if person == None: 
-        return JSONResponse(
-                status_code=status.HTTP_404_NOT_FOUND, 
-                content={ "message": "Пользователь не найден" }
-        )
-    # если пользователь найден, изменяем его данные и отправляем обратно клиенту
-    person.age = data["age"]
-    person.name = data["name"]
-    return person
- 
- 
-@app.delete("/api/users/{id}")
-def delete_person(id):
-    # получаем пользователя по id
-    person = find_person(id)
-  
-    # если не найден, отправляем статусный код и сообщение об ошибке
-    if person == None:
-        return JSONResponse(
-                status_code=status.HTTP_404_NOT_FOUND, 
-                content={ "message": "Пользователь не найден" }
-        )
-  
-    # если пользователь найден, удаляем его
-    people.remove(person)
-    return person
+@app.post("/login")
+def login(data = Body()):
+    try:
+        if check_username(data['login']) and check_user_password(data['pass']):
+            return True
+        else:
+            return False
+    except Exception:
+        return Response(content = "Sorry, something went wrong\n", media_type="text/plain")
 
-def requ(one, two):
-    req = requests.get("http://127.0.0.1:5000/")
-    print(req.status_code)
-    js = {}
-    js['first'] = one
-    js['two'] = two
-    req = requests.post(url = "http://127.0.0.1:5000/calc",json = js)
-    req = req.json()
-    return req['response']
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8052)
---
+++ pages/login.py
@@ -0,0 +1,122 @@
+import dash
+from dash import html, callback, Output, Input, State, dcc
+import dash_bootstrap_components as dbc
+from dash.exceptions import PreventUpdate
+from dash_iconify import DashIconify
+import dash_mantine_components as dmc
+from utils.work_with_database import check_username, check_user_password
+import requests
+
+dash.register_page(__name__, path_template="/login")
+
+
+def login_layout():
+    return dbc.Container([
+                dbc.Col(html.Div(["Please log in to continue:"],
+                                 style={'paddingTop': '100px', 'paddingBottom': '20px'}),
+                        width={'size': 4, 'offset': 4}),
+                dbc.Col([dbc.Label(["Username"], html_for="example-email-grid"),
+                         dbc.Input(type="text", n_submit=0, id="uname-box",
+                                   placeholder=['Enter your username']),
+                         ], width={'size': 4, 'offset': 4}
+                        ),
+                dbc.Col([html.Div(children=
+                                  [dbc.Input(
+                                            type="password", n_submit=0, id="pwd-box",
+                                            placeholder='Enter your password',
+                                            style={'background-color': 'transparent', 'width': 'calc(100% - 50px)',
+                                                   'display': 'inline', 'box-shadow': 'none'}
+                                            ),
+                                   dmc.Button(children=[DashIconify(icon="ant-design:eye-outlined",
+                                                                    width=30, color='#7a8288',
+                                                                    id='pwd-box-icon')],
+                                              style={'background-color': 'transparent',
+                                                     'vertical-align': 'middle'}, compact=True,
+                                              id='pwd-box-icon-button')
+                                  ],
+                                  style={'padding': '0px'},
+                                  className='form-control'),
+                         ], width={'size': 4, 'offset': 4}
+                        ),
+                dbc.Col(dbc.Button(children=["Login"], n_clicks=0, type='submit', id='login-button',
+                                   style={'marginTop': '20px'}),
+                        width={'size': 4, 'offset': 4}),
+                dbc.Col(html.Div(children='', id='output-state'), style={'marginTop': '20px'},
+                        width={'size': 4, 'offset': 4}),
+
+            dbc.Col([html.A(["Don't have an account?"]),
+                     html.A(children=["Registration"], href='/registration')
+                     ], width={'size': 4, 'offset': 4})], id='login-main-container')
+
+
+
+layout = dbc.Container([dcc.Location(id='login-login', refresh=True), html.Div([], id='login-layout')])
+
+@callback(Output('user-name', 'data'),
+          Output('login-login', 'pathname'),
+          [Input('login-button', 'n_clicks'),
+           Input('uname-box', 'n_submit'),
+           Input('pwd-box', 'n_submit')],
+          [State('uname-box', 'value'),
+           State('pwd-box', 'value')])
+def login(n_clicks, n_submit_uname, n_submit_pwd, input1, input2):
+    if check_username(input1):
+        if check_user_password(input1, input2):
+            return input1, '/'
+        else:
+            return None, '/login'
+    else:
+        return None, '/login'
+
+
+@callback(Output('output-state', 'children'),
+          [Input('login-button', 'n_clicks'),
+           State('uname-box', 'n_submit'),
+           State('pwd-box', 'n_submit'),
+           State('uname-box', 'value'),
+           State('pwd-box', 'value')])
+def update_output(n_clicks, n_submit_uname, n_submit_pwd, input1, input2):
+
+    if n_clicks > 0 or n_submit_uname > 0 or n_submit_pwd > 0:
+        user = {}
+        user['login'] = input1
+        user['pass'] = input2
+        result = requests.post(url='http://127.0.0.1:8052/login', json=user)
+        if result == True:
+            return ''
+        else:
+            return dbc.Alert('Incorrect username or password!', color="danger")
+        """if check_username(user):
+            if check_user_password(user, input2):
+                return ''
+            else:
+                return dbc.Alert('Incorrect username or password!', color="danger")
+        else:
+            return dbc.Alert('Incorrect username or password!', color="danger")"""
+    else:
+        return ''
+    
+@callback(
+    Output('login-layout', 'children'),
+    Input('user-name', 'modified_timestamp'),
+    State('user-name', 'data'))
+def change_lang2(ts, username):
+    if ts is None:
+        raise PreventUpdate
+    return login_layout()
+
+@callback(
+    Output('pwd-box', 'type'),
+    Output('pwd-box-icon', 'icon'),
+    Input('pwd-box-icon-button', 'n_clicks'),
+    State('pwd-box', 'type'),
+    prevent_initiall_call=True
+)
+def hide_pass_1(n_clicks, type_):
+    if n_clicks is not None:
+        if type_ == 'password':
+            return 'text', 'ant-design:eye-invisible-outlined'
+        else:
+            return 'password', 'ant-design:eye-outlined'
+    else:
+        return dash.no_update, dash.no_update
